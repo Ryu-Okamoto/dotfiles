53687305c8af3eb529a9083cf99eb507cad00d9e
Ryu-Okamoto <r-okamot@ist.osaka-u.ac.jp>
2024-07-25 15:01:15 +0900

    init

---
+++ dotfiles/.vimrc
@@ -0,0 +1,6 @@
+set number
+
+let g:dracula_italic = 0
+packadd! dracula
+syntax enable
+colorscheme dracula
---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+function __link() {
+    [ -L "$2" ] && unlink "$2"
+    ln -s "$1" "$2" && echo "linked: $1 -> $2" >&2
+}
+
+dotfiles_root=$(cd $(dirname $0) && pwd)/dotfiles
+cd ${dotfiles_root}
+
+dotfiles=`ls -a | grep -E '^\.[^.]+'` 
+for dotfile in $dotfiles
+do
+    full_path="$dotfiles_root/$dotfile" 
+    link_name="$HOME/$dotfile"
+    __link $full_path $link_name    
+done
